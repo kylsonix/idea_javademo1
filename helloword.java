e1b803dd5199e62bd3fa1059f650c5f0eda329ef
kylsonix <1569868187@qq.com>
2026-07-03 22:56:33 +0800

    Add files via upload

---
+++ helloword.java
@@ -0,0 +1,12 @@
+package test;
+//class表示定义一个类，类是Java项目中最基本的组成单元
+//
+public class helloword {
+
+    //main表示Java程序的主入口
+    public static void main(String[] args) {
+//        输出语句，输出hello world
+        System.out.println("hello world");
+    }
+}
+
