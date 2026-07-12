8dc4d4f07980d12224aaae389325fd5de354b8ae
kylsonix <1569868187@qq.com>
2026-07-12 20:02:44 +0800

    Add files via upload

---
+++ FinalDemo1/Test.java
@@ -0,0 +1,11 @@
+package FinalDemo1;
+
+public class Test {
+    public static void main(String[] args) {
+        //final
+    }
+}
+//final，终极
+//1、final修饰类，类不能被继承
+//2、final修饰方法，方法不能被重写
+//3、final修饰变量，变量有且只能被赋值一次
